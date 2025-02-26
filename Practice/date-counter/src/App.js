17d63f20f1d71aab5c0b2bb5c177d45a6a30b7e4
vigneshayyappan-dev <ivigneshayyappan@gmail.com>
2025-02-26 19:33:16 +0530

    Add date-counter as a regular folder

---
+++ Practice/date-counter/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+// function Counter() {}
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date(); 
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count:{count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is`
+              : `${count} days ago was`}
+          </span>
+          {date.toLocaleString()}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
