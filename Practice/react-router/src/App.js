2b7ec92365f9c434807c51cf72c9cf6fa81dc003
vigneshayyappan-dev <ivigneshayyappan@gmail.com>
2025-02-26 19:36:49 +0530

    Added folder for React-router

---
+++ Practice/react-router/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import User from "./pages/User";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import RouterHelper from "./components/RouterHelper";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <RouterHelper />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/users/user/:username" element={<User />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
